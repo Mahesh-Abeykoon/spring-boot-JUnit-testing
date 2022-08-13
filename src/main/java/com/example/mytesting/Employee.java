4940ff0e9a3a69e86ded98742e3fa6ebd427b3a9
“Mahesh <maheshpabeykoon@gmail.com?>
2022-08-13 16:49:27 +0530

    Intial first commit and push employee managemt system with JUnit testing

---
+++ src/main/java/com/example/mytesting/Employee.java
@@ -0,0 +1,33 @@
+package com.example.mytesting;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name ="employees")
+@Entity
+public class Employee {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+}
---
+++ src/main/java/com/example/mytesting/MyTestingApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestingApplication.class, args);
+	}
+
+}
